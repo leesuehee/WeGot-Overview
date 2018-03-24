91aa7fc47440a79e48dcde70cb508f255da8c85e
leesuehee <leesuehee@outlook.com>
2018-03-23 17:56:56 -0700

    Renders view and serves data from specific id

--- server/serverPost.js
+++ server/serverPost.js
@@ -1,7 +1,4 @@
-const mongoose = require('mongoose');
 const papp = require('./papp');
-
-const dbAddress = process.env.DB_ADDRESS || 'localhost';
 
 papp.listen(3002, () => {
   console.log('Listening on port 3002');
